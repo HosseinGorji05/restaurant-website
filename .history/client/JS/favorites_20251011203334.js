9c871309ced9e39b3368995695174026fc373ffa
Hossein Gorji <hoseingorji1383@gmail.com>
2026-06-04 00:00:59 -0400

    chore: remove .history from tracking

--- .history/client/JS/favorites_20251011203334.js
+++
@@ -1,5 +0,0 @@
-const menuResponse = await fetch('http://localhost:3000/menu/items'); // makes the request to the server
-const menuData = await menuResponse.json();  // converts the response body from json to Js object 
-menuData.items.forEach(element => {
-    console.log(item.name);
-});
